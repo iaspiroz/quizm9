3698496c6b261b54eca4edab41cbecf345acc3dc
Ignacio Aspiroz Argüelles <iaspiroz@gmail.com>
2015-07-31 19:14:17 +0200

    Módulo 8 - Editar preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,12 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
 
 
 router.get('/author',function(req,res){
-  res.render('author',{author:'Ignacio Aspiroz Argüelles',photo:'images/fry.png',video:'images/homer.mp4'});
+  res.render('author',{author:'Ignacio Aspiroz Argüelles',photo:'images/fry.png',video:'images/homer.mp4',errors:[]});
 });
 
 module.exports = router;
